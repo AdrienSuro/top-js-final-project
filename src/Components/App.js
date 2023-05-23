d98e164a1a5798cf3daaa65c7ce96b17a8ec58f7
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-05-23 19:09:49 +0200

    can grab objects from collection

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,34 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import "../Stylesheets/App.scss";
 import "../Stylesheets/normalize.scss";
 import Tweet from "./Tweet";
 import WriteTweet from "./WriteTweet";
-import tweetsCollection from "./Firebase";
+import { tweetsCollection, db } from "./Firebase";
+import {
+  doc,
+  onSnapshot,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const allTweets = collection(db, "tweets");
+      const q = query(allTweets, where("userName", "==", "adrien_surowiec"));
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.data());
+      });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <body>
@@ -23,6 +47,6 @@ function App() {
       </body>
     </div>
   );
-}
+};
 
 export default App;

e02da02003acfd5cbcad9ddbd5df84c359b18280
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-05-25 21:08:59 +0200

    created date display and random stats

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import rushdie from "../img/rushdie.jpg";
 import "../Stylesheets/normalize.scss";
 import { ReactSVG } from "react-svg";
@@ -8,24 +9,40 @@ import { ReactComponent as HeartSVG } from "../icons/heart.svg";
 import { ReactComponent as StatsSVG } from "../icons/stats.svg";
 import { v4 as uuidv4 } from "uuid";
 
-export default function Tweet(props) {
-  // function getTweetDate(date) {
-  //   const currentDate = new Date().getTime();
+function Tweet(props) {
+  function getDate() {
+    let currentDate = new Date().getTime();
+    let tweetDate = props.timestamp.toDate().getTime();
+    let diff = currentDate - tweetDate;
+    console.log("inside getDate");
+    if (diff < 1000) {
+      return "0s";
+    } else if (diff < 60000) {
+      return parseInt(diff / 1000) + "s";
+    } else if (diff < 3600000) {
+      return parseInt(diff / 60000) + "m";
+    } else if (diff < 86400000) {
+      return parseInt(diff / 360000) + "h";
+    } else {
+      return props.timestamp.toDate().toLocaleString();
+    }
+  }
 
-  //   if ((date.toDate().getDate() - currentDate))
-  // }
+  function getRandomNum() {
+    return Math.floor(Math.random() * 999);
+  }
 
   return (
     <div>
-      {console.log(props.timestamp.toDate().getDate())}
+      {/* {console.log(props.timestamp.toDate().getDate())}
       {console.log(new Date())}
-      {console.log(props.timestamp.toDate().getDate() - new Date())}
+      {console.log(props.timestamp.toDate().getDate() - new Date())} */}
       <div className="tweet">
         <img src={rushdie}></img>
         <div className="tweetAuthor">
           <p className="authorName">{props.name}</p>
           <p className="authorUsername">{props.userName}</p>
-          <p className="tweetTime">· {props.timestamp.toDate().getDate()}</p>
+          <p className="tweetTime">· {getDate()}</p>
         </div>
         <div className="tweetContent">
           <p>{props.content}</p>
@@ -34,21 +51,23 @@ export default function Tweet(props) {
           <div id="commentIcon">
             <CommentSVG id="commentSVG" />
           </div>
-          <span id="commentSpan">23</span>
+          <span id="commentSpan">{getRandomNum()}</span>
           <div id="retweetIcon">
             <RetweetSVG id="retweetSVG" />
           </div>
-          <span id="retweetSpan">55</span>
+          <span id="retweetSpan">{getRandomNum()}</span>
           <div id="heartIcon">
             <HeartSVG id="heartSVG" />
           </div>
-          <span id="heartSpan">229</span>
+          <span id="heartSpan">{getRandomNum()}</span>
           <div id="statsIcon">
             <StatsSVG id="statsSVG" />
           </div>
-          <span id="statsSpan">1.3K</span>
+          <span id="statsSpan">{getRandomNum()}</span>
         </div>
       </div>
     </div>
   );
 }
+
+export default Tweet;

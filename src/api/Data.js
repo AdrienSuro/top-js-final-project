63c0080a4e2bf3d3b0892fca1b460be4487e8e36
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-07-24 16:13:05 +0200

    listed all future back-end related functions

---
+++ src/api/Data.js
@@ -0,0 +1,37 @@
+// ******* READ ******* //
+
+export function getOwnTweets(userName) {
+  //will get user Tweets
+}
+
+export function getRetweets(userName) {
+  //will get user Tweets
+}
+
+export function getOwnTweetsAndRetweets(userName) {
+  // will get both Tweets and Retweets
+}
+
+export function getProfileData(userName) {
+  // will get user Info as an object
+}
+
+export function getTimeline(userName) {}
+
+// ******* WRITE ******* //
+
+export function createNewUser(userObject) {
+  // will create a New User
+}
+
+export function changeUserDescription(userName, newDescription) {
+  return;
+}
+
+export function changeUserLocation(userName, newLocation) {
+  return;
+}
+
+export function changeUserDisplayName(userName, newDisplayName) {
+  return;
+}

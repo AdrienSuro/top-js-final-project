1abd70d377a785bd0c9153861fdfb5febea75fb8
Adrien Surowiec <114209079+AdrienSuro@users.noreply.github.com>
2023-08-01 08:53:26 +0000

    Fixed LoginPage.js

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -7,15 +7,23 @@ export default function LoginPage() {
   return (
     <div>
       <h1>Login Page</h1>
-      <button onClick={connectWithGoogle()}>Connect with Google</button>
+      <button
+        onClick={() => {
+          connectWithGoogle();
+        }}
+      >
+        Connect with Google
+      </button>
       <h1>OR</h1>
       <input type="text" name="emailField" id="emailField"></input>
       <input type="text" name="passwordField" id="passwordField"></input>
       <button
-        onClick={connectWithEmail(
-          document.getElementById("emailField"),
-          document.getElementById("passwordField")
-        )}
+        onClick={() => {
+          connectWithEmail(
+            document.getElementById("emailField"),
+            document.getElementById("passwordField")
+          );
+        }}
       >
         Connect with Email
       </button>
